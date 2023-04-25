e8b361d6eac5bff9e35b0eaacaf113df6686c0d2
tarnprp <tarnprp@hotmail.com>
2023-04-25 13:59:04 +0700

    Create Exercise4_Pornrapat_W.py

---
+++ Exercise4_Pornrapat_W.py
@@ -0,0 +1,9 @@
+fe = 84
+gb = 65
+ic = 43
+cp = 12
+print("YOUR SCORE")
+print("Foundation English",":",fe)
+print("General Business",":",gb)
+print("Introduction to Computer Systems", ":", ic)
+print("Computer Programming", ":", cp)
